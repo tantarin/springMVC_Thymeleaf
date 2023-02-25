179fa0cc8cb16699d4c00d30cbe0edd6200f35bf
Tanya Tarinskaya <tanya.tarinskaya@gmail.com>
2023-02-26 01:04:18 +0300

    add log

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.entity.Customer;
 import com.example.demo.repository.CustomerRepository;
 import com.example.demo.service.CustomerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +18,8 @@ import java.util.Optional;
 @Controller
 @RequestMapping("/")
 public class CustomerController {
+
+    Logger logger = LoggerFactory.getLogger(CustomerController.class);
     private final CustomerService customerService;
     private final CustomerRepository customerRepository;
 
@@ -27,6 +31,7 @@ public class CustomerController {
 
     @GetMapping
     public String getAllCustomers(Model model){
+        logger.info("getting all customers");
         List<Customer> customers = customerService.getAll();
         model.addAttribute("customers", customers);
         return "customers";
@@ -51,9 +56,21 @@ public class CustomerController {
     }
 
     @GetMapping(path = "/delete/{id}")
-    public String deleteCustomerById(Model model,
-                                 @PathVariable("id") Long id) {
+    public String deleteCustomerById(@PathVariable("id") Long id) {
+        logger.info("deleting the customer, Id:" + id);
         customerRepository.deleteById(id);
         return "redirect:/";
+    }
+
+    @RequestMapping("/log")
+    @ResponseBody
+    public String index() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+
+        return "Check out the Logs to see the output...";
     }
 }
